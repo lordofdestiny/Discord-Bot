597832800717de809b23ea82b319df5b9ef9d659
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2018-09-02 04:09:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const WarframeVersion = require("warframe-updates");
 const warframeVersion = new WarframeVersion();
 const bot = new Discord.Client({ disableEveryone: true });
 
-const prefix = process.env.token || botSettings.prefix;
+const prefix = process.env.token/* || botSettings.prefix*/;
 
 const commands = {
   userinfo: "Get your info:",
@@ -144,4 +144,4 @@ bot.on("message", async message => {
   }
 });
 
-bot.login(process.env.token || botSettings.token);
+bot.login(process.env.token/* || botSettings.token*/);
