61df9c932c1ef0450e9e17cc2c957813fa76beb8
Djumic Branislav <djumicbranislav@outlook.com>
2018-09-03 03:31:36 +0200

    Merged commanFormat and commands, fixed !help, removed !userinfo, added tts to !didThanosKillMe, added !alerts

--- bot.js
+++ bot.js
@@ -1,34 +1,50 @@
-const botSettings = require("./botSettings.json");
+require("dotenv").config();
 const Discord = require("discord.js");
 const moment = require("moment");
 const myTools = require("./myTools");
 const axios = require("axios");
 const WarframeVersion = require("warframe-updates");
 const warframeVersion = new WarframeVersion();
+const WorldState = require("warframe-worldstate-parser");
 const bot = new Discord.Client({ disableEveryone: true });
+let worldState;
 
-const prefix = botSettings.prefix || process.env.token;
+const prefix = process.env.prefix;
 
 const commands = {
-  userinfo: "Get your info:",
-  talk: "Bot says what you tell him to:",
-  joke: "Bot tells you a joke: ",
-  didThanosKillMe: "Tells you if Thanos killed you with his snap",
-  update: "Gives you lates update info!"
-};
-
-const commandFormat = {
-  userinfo: "",
-  talk: "<text> (tts)",
-  joke: "(tts)",
-  didThanosKillMe: "",
-  update: ""
+  // userinfo: {
+  //   info: "Get your info:",
+  //   add: ""
+  // },
+  talk: {
+    info: "Bot says what you tell him to:",
+    add: "<text> (tts)"
+  },
+  joke: {
+    info: "Bot tells you a joke: ",
+    add: "(tts)"
+  },
+  didThanosKillMe: {
+    info: "Tells you if Thanos killed you with his snap",
+    add: "(tts)"
+  },
+  update: {
+    info: "Gives you lates Warframe update info!",
+    add: ""
+  },
+  alerts: {
+    info: "Gives you list of currsnt alerts!",
+    add: ""
+  }
 };
 
 const color = "#F04747";
 
 const dragonThumbnail =
   "https://orig00.deviantart.net/2d14/f/2014/206/8/3/dragon_portrait_by_aazure_dragon-d7s7qqi.png";
+
+const footerIcon =
+  "https://community.gophersvids.com/uploads/monthly_2015_07/Warframe_Logo_v1.png.66fa77f0af4dadc0b1a269016e9ab36a.png";
 
 bot.on("ready", async () => {
   bot
@@ -39,6 +55,7 @@ bot.on("ready", async () => {
     .catch(error => {
       console.log(error.stack);
     });
+  updateWorldState();
 });
 
 bot.on("message", async message => {
@@ -65,8 +82,8 @@ bot.on("message", async message => {
       );
     Object.keys(commands).forEach(key => {
       embed.addField(
-        commands[key],
-        "```" + `${prefix}${key}` + ` ${commandFormat[key]}` + "```"
+        commands[key].info,
+        "```" + `${prefix}${key}` + ` ${commands[key].add}` + "```"
       );
     });
     message.channel.send(embed);
@@ -83,28 +100,28 @@ bot.on("message", async message => {
     return;
   }
 
-  if (command === `${prefix}userinfo`) {
-    //if user aks for his info
-    let embed = new Discord.RichEmbed()
-      .setAuthor(message.author.username)
-      .setDescription(`This is the ${message.author.username}'s info!`)
-      .setColor(color)
-      .setThumbnail(message.author.avatarURL)
-      .addField(
-        "Full Username",
-        `${message.author.username}#${message.author.discriminator}`
-      )
-      .addField("ID", message.author.id)
-      .addField(
-        "Created At",
-        moment(
-          message.author.createdAt,
-          "ddd MMM DD YYYY HH:mm:ss GMT+-HHmm"
-        ).format("dddd, MMMM Do YYYY.")
-      );
-    message.channel.send(embed);
-    return;
-  }
+  // if (command === `${prefix}userinfo`) {
+  //   //if user aks for his info
+  //   let embed = new Discord.RichEmbed()
+  //     .setAuthor(message.author.username)
+  //     .setDescription(`This is the ${message.author.username}'s info!`)
+  //     .setColor(color)
+  //     .setThumbnail(message.author.avatarURL)
+  //     .addField(
+  //       "Full Username",
+  //       `${message.author.username}#${message.author.discriminator}`
+  //     )
+  //     .addField("ID", message.author.id)
+  //     .addField(
+  //       "Created At",
+  //       moment(
+  //         message.author.createdAt,
+  //         "ddd MMM DD YYYY HH:mm:ss GMT+-HHmm"
+  //       ).format("dddd, MMMM Do YYYY.")
+  //     );
+  //   message.channel.send(embed);
+  //   return;
+  // }
 
   if (command === `${prefix}talk`) {
     myTools.deleteMessage(message);
@@ -115,7 +132,10 @@ bot.on("message", async message => {
   }
 
   if (command === `${prefix}didThanosKillMe`) {
-    message.channel.send(myTools.snap());
+    let doTts = args[args.length - 1] === "tts";
+    message.channel.send(`<@${message.author.id}> ${myTools.snap()}`, {
+      tts: `${doTts}`
+    });
     return;
   }
 
@@ -152,10 +172,63 @@ bot.on("message", async message => {
       .setThumbnail(
         "https://www.bleedingcool.com/wp-content/uploads/2017/12/warframe-Logo.jpg"
       );
-    console.log(update.version);
     if (update.version !== "") embed.addField("Version", update.version);
     message.channel.send(embed);
   }
+
+  if (command == `${prefix}alerts`) {
+    updateWorldState();
+    for (let pos in worldState.alerts) {
+      let alert = worldState.alerts[pos];
+      let str = alert.mission.reward.asString;
+      let inds = str.indexOf("+");
+      let trueRew = inds > -1 ? str.substring(0, inds - 1) : str;
+
+      console.log(str);
+      let embed = new Discord.RichEmbed()
+        .setColor(myTools.rbgIntToRgb(alert.mission.reward.color))
+        .setThumbnail(alert.mission.reward.thumbnail)
+        .setTitle("[PC] " + trueRew)
+        .addField(
+          "Mission",
+          alert.mission.faction + " " + alert.mission.type,
+          true
+        )
+        .addField("Loaction", alert.mission.node, true)
+        .addField(
+          "Level",
+          `${alert.mission.minEnemyLevel} - ${alert.mission.maxEnemyLevel}`,
+          true
+        )
+        .addField(
+          "Archwing Required: ",
+          alert.mission.archwingRequired ? "Yes" : "No",
+          true
+        )
+        .addField("\u200B", `**Credits: ** ${alert.mission.reward.credits}`)
+        .setFooter(
+          "Page " +
+            `${parseInt(pos) + 1}` +
+            `/${worldState.alerts.length} \u2022 ` +
+            alert.eta +
+            " remaining",
+          footerIcon
+        )
+        .setTimestamp();
+      message.channel.send(embed);
+    }
+  }
 });
 
-bot.login(process.env.token || botSettings.token);
+updateWorldState = () => {
+  axios
+    .get("http://content.warframe.com/dynamic/worldState.php")
+    .then(res => {
+      worldState = new WorldState(JSON.stringify(res.data));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+bot.login(process.env.token);
