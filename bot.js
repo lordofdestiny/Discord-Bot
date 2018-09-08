e6890836446f38d40087f5204437367309578f24
Djumic Branislav <djumicbranislav@outlook.com>
2018-09-08 10:14:45 +0200

    Added xp and hate system, !baro command, moved commands to external file

--- bot.js
+++ bot.js
@@ -1,58 +1,25 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const moment = require("moment");
+const sql = require("sqlite");
 const myTools = require("./Helpers/myTools");
 const axios = require("axios");
 const WarframeVersion = require("warframe-updates");
 const warframeVersion = new WarframeVersion();
 const generateEmbed = require("./Helpers/generate");
 const WorldState = require("warframe-worldstate-parser");
+const handleInteractives = require("./Helpers/handleIntereactives");
+const commands = require("./commands.json");
 let worldState;
-
 const bot = new Discord.Client({ disableEveryone: true });
-
 const prefix = process.env.prefix;
-
 const dragonThumbnail =
   "https://orig00.deviantart.net/2d14/f/2014/206/8/3/dragon_portrait_by_aazure_dragon-d7s7qqi.png";
 const footerIcon =
   "https://community.gophersvids.com/uploads/monthly_2015_07/Warframe_Logo_v1.png.66fa77f0af4dadc0b1a269016e9ab36a.png";
 const color = "#F04747";
 
-const commands = {
-  talk: {
-    info: "Bot says what you tell him to:",
-    add: "<text> *tts"
-  },
-  joke: {
-    info: "Bot tells you a joke: ",
-    add: "*tts"
-  },
-  didThanosKillMe: {
-    info: "Tells you if Thanos killed you with his snap",
-    add: "*tts"
-  },
-  // update: {
-  //   info: "Gives you info on lates Warframe update!",
-  //   add: ""
-  // },
-  alerts: {
-    info: "List currsnt Alerts!",
-    add: ""
-  },
-  invasions: {
-    info: "List current Invasions!",
-    add: ""
-  },
-  sortie: {
-    info: "Current sortie missions!",
-    add: ""
-  },
-  sorry: {
-    info: "Fixes sams hate for you!",
-    add: ""
-  }
-};
+sql.open("./database.sqlite");
 
 bot.on("ready", async () => {
   bot
@@ -72,17 +39,156 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let command = messageArray[0];
   let args = messageArray.slice(1);
+  var flag = false;
+
+  await sql //Load hate
+    .get(`SELECT * FROM hates WHERE userId = "${message.author.id}"`)
+    .then(row => {
+      if (row.hated) {
+        flag = true;
+      }
+    })
+    .catch(erorr => {
+      sql
+        .run(
+          "CREATE TABLE IF NOT EXISTS hates (userId TEXT, warned BOOLEAN, hated BOOLEAN)"
+        )
+        .then(() => {
+          console.log("Created table");
+        })
+        .catch(error => {
+          console.log("Error while creating hates table");
+        });
+    });
 
   if (message.isMentioned(bot.user)) {
-    message.reply("Don't do that ever again!!!  (╯°□°）╯︵ ┻━┻");
+    sql
+      .get(`SELECT * FROM hates WHERE userId = "${message.author.id}"`)
+      .then(row => {
+        if (!row) {
+          sql
+            .run("INSERT INTO hates (userId, warned, hated) VALUES (?, ?, ?)", [
+              message.author.id,
+              true,
+              false
+            ])
+            .then(() => {
+              message.reply("Don't do that ever again!!!  (╯°□°）╯︵ ┻━┻");
+            })
+            .catch(() => {
+              console.log("Error while first warning!");
+            });
+        } else if (row.hated) {
+          message.reply("¯\\_(ツ)_/¯");
+        } else if (row.warned) {
+          sql
+            .run(
+              `UPDATE hates SET warned = ${true}, hated = ${true} WHERE userId = ${
+                message.author.id
+              } `
+            )
+            .then(() => {
+              message.reply(
+                "(╯°□°）╯︵ ┻━━━━━┻ I've warned you already!!!\n I'll ignore you in the future!"
+              );
+            })
+            .catch(() => {
+              console.log("Error while hating!");
+            });
+        } else if (!row.warned) {
+          sql
+            .run(
+              `UPDATE hates SET warned = ${true}, hated = ${false} WHERE userId = ${
+                message.author.id
+              } `
+            )
+            .then(() => {
+              message.reply("Don't do that ever again!!!  (╯°□°）╯︵ ┻━┻");
+            })
+            .catch(() => {
+              console.log("Error while adding warning!");
+            });
+        }
+      });
+    return;
   }
+
+  sql //Load scores
+    .get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`)
+    .then(row => {
+      if (!row) {
+        sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [
+          message.author.id,
+          1,
+          0
+        ]);
+      } else {
+        let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+        if (curLevel > row.level) {
+          row.level = curLevel;
+          sql.run(
+            `UPDATE scores SET points = ${row.points + 1}, level = ${
+              row.level
+            } WHERE userId = "${message.author.id}"`
+          );
+          message.reply(
+            `You've leveled up to level **${curLevel}**! Ain't that dandy?`
+          );
+        }
+        sql.run(
+          `UPDATE scores SET points = ${row.points + 1} WHERE userId = "${
+            message.author.id
+          }"`
+        );
+      }
+    })
+    .catch(() => {
+      sql
+        .run(
+          "CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)"
+        )
+        .then(() => {
+          sql.run(
+            "INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)",
+            [message.author.id, 1, 0]
+          );
+        });
+    });
 
   if (!command.startsWith(prefix)) {
     return;
   }
 
   if (command === `${prefix}sorry`) {
-    message.reply("You saved yourself. ┬─┬ ノ( ゜-゜ノ)");
+    sql
+      .get(`SELECT * FROM hates WHERE userId = "${message.author.id}" `)
+      .then(row => {
+        if (!row || (!row.hated && !row.warned)) {
+          message.reply("You have nothing to be sorry for. 😉");
+        } else if (row.hated) {
+          sql
+            .run(
+              `UPDATE hates SET warned = ${false}, hated = ${false} WHERE userId = "${
+                message.author.id
+              }"`
+            )
+            .then(() => {
+              console.log("Successful!");
+              message.reply("You saved yourself. ┬─┬ ノ( ゜-゜ノ)");
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        } else if (row.warned) {
+          message.reply("It's just a warning, no sorry yet!");
+        }
+      });
+    return;
+  }
+
+  if (flag) {
+    message.reply("¯\\_(ツ)_/¯");
+    return;
   }
 
   if (command === `${prefix}help`) {
@@ -94,7 +200,7 @@ bot.on("message", async message => {
 
   if (!(command.slice(1) in commands)) {
     //if users enters unexisting command
-    message.reply(`¯\\_(ツ)_/¯ To get list of commands type ${prefix}help`);
+    message.reply(`To get list of commands type ${prefix}help`);
     return;
   }
 
@@ -152,55 +258,20 @@ bot.on("message", async message => {
   //   message.channel.send(embed);
   // }
 
-  if (command == `${prefix}alerts`) {
-    console.log("waiting!");
+  if (command === `${prefix}profile`) {
+    sql
+      .get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`)
+      .then(async row => {
+        if (!row) return message.reply("Your current level is 0");
+        let embed = await generateEmbed.userEmbed(row, message.author);
+
+        message.channel.send(embed);
+      });
+  }
+
+  if (command === `${prefix}alerts`) {
     await updateWorldState();
-    console.log("Done!");
-    let pos = 0;
-    let embed = await generateEmbed.alertEmbed(pos, worldState); //Generate alert embed
-    message.channel.send(embed).then(async sentMessage => {
-      //Add nav reactions
-      await sentMessage.react("⏮");
-      await sentMessage.react("◀");
-      await sentMessage.react("▶");
-      await sentMessage.react("⏭");
-      const filter = (
-        //check only for those emoji's
-        reaction,
-        user
-      ) =>
-        (reaction.emoji.name === "⏮" ||
-          reaction.emoji.name === "◀" ||
-          reaction.emoji.name === "▶" ||
-          reaction.emoji.name === "⏭") &&
-        user.id != bot.user.id;
-      const collector = await sentMessage.createReactionCollector(filter, {
-        //Create reaction collector for 60s
-        time: 60000
-      });
-      collector.on("collect", async reaction => {
-        //Decide on stuff
-        let name = reaction.emoji.name;
-        console.log(name);
-        if (name === "⏮") {
-          pos = 0;
-          sentMessage.edit(await generateEmbed.alertEmbed(pos, worldState));
-        } else if (name === "◀") {
-          if (pos > 0) {
-            pos--;
-            sentMessage.edit(await generateEmbed.alertEmbed(pos, worldState));
-          }
-        } else if (name === "▶") {
-          if (pos < worldState.alerts.length - 1) pos++;
-          sentMessage.edit(await generateEmbed.alertEmbed(pos, worldState));
-        } else if (name === "⏭") {
-          pos = worldState.alerts.length - 1;
-          sentMessage.edit(await generateEmbed.alertEmbed(pos, worldState));
-        }
-        reaction.remove(message.author);
-      });
-      collector.on("end", collected => {}); //exit
-    });
+    handleInteractives.handleAlerts(message, worldState, bot.user.id);
     return;
   }
 
@@ -208,12 +279,28 @@ bot.on("message", async message => {
     await updateWorldState();
     let embed = await generateEmbed.invasionEmbed(worldState);
     message.channel.send(embed);
+    return;
   }
 
   if (command === `${prefix}sortie`) {
     await updateWorldState();
     let embed = await generateEmbed.sortieEmbed(worldState);
     message.channel.send(embed);
+    return;
+  }
+
+  if (command === `${prefix}fissures`) {
+    await updateWorldState();
+    let embed = await generateEmbed.fissuresEmbed(worldState);
+    message.channel.send(embed);
+    return;
+  }
+
+  if (command === `${prefix}baro`) {
+    await updateWorldState();
+    let embed = await generateEmbed.BaroEmbed(worldState);
+    message.channel.send(embed);
+    return;
   }
 });
 
